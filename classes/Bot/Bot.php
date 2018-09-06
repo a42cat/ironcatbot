56b4ed578074369bb39a9356cf6e1a14fc962dae
phantom <phantomkemerovo@mail.ru>
2018-09-06 20:33:06 +0300

    тестовый запуск бота

--- classes/Bot/Bot.php
+++ classes/Bot/Bot.php
@@ -9,14 +9,29 @@
 namespace IronCatBot\Classes\Bot;
 use IronCatBot\Classes\Config\ConfigOther as CFG;
 use IronCatBot\Classes\Log\Log;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Client;
 
 class Bot
 {
     
     public static function Init()
     {
-        
-        echo 'test bot';
+        $bot = new BotApi(CFG::GetToken());
+        $client = new Client(CFG::GetToken());
+        $client->command('start', function ($message) use ($bot) {
+            $answer = 'Добро пожаловать!';
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+        // команда для помощи
+        $client->command('help', function ($message) use ($bot) {
+            $answer = 'Команды:
+        /help - вывод справки';
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+    
+        $client->run();
         
     }
     
