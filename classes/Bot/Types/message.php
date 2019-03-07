a7e14f52f60b853557447181d5314a7c7687ffdf
Kislicin Alexey <phantomkemerovo@mail.ru>
2019-03-07 16:51:08 +0300

    Отладка

--- classes/Bot/Types/message.php
+++ classes/Bot/Types/message.php
@@ -14,9 +14,7 @@ use IronCatBot\Classes\Bot\Config\Config;
 class Message
 {
     static function sendMessage($text, $chatid, $messageid , $preview) {
-        
-        echo 'answer message for: ' . $text;
-    
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, Config::getUrlApi() . '/sendMessage?chat_id=' . $chatid . '&reply_to_message_id='. $messageid .'&parse_mode=HTML&text=' . urlencode($text)."&disable_web_page_preview=".$preview);
         curl_setopt($ch, CURLOPT_HEADER, 0);
