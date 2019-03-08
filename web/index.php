6adcc173bf85281184abdd2d23e99a6a072d2f30
Kislicin Alexey <phantomkemerovo@mail.ru>
2019-03-09 00:06:37 +0300

    Добавлен метод отправки лога в канал

--- web/index.php
+++ web/index.php
@@ -12,6 +12,8 @@ try {
     var_dump($bot::getJson());
     var_dump(2);
     Log::SendLog(json_encode($bot::getJson()), 'json');
+    var_dump(3);
+    Message::sendMessage('ответ',$bot::getJson()['message']['chat']['id'],null,true);
 
 } catch (Exception $e) {
     echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
