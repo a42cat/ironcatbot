dc356ec928950f69432ae92ef6966a02a9954bd7
Kislicin Alexey <phantomkemerovo@mail.ru>
2019-03-09 00:56:46 +0300

    Отладка

---
+++ classes/IronCatBot/Router/Router.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 08.09.2018
+ * Time: 17:10
+ */
+
+namespace IronCatBot\Router;
+
+use IronCatBot\Debug\Debug;
+use IronCatBot\IronCatBot\ICBot;
+use IronCatBot\IronCatBot\Types\Message;
+
+class Router extends ICBot
+{
+    public function ExecCmd($json)
+    {
+        $cmd = explode(' ', $json['message']['text']);
+        $cmd[0] = strtolower($cmd[0]);
+        $cmd[0] = str_replace('@ironcatbot', '', $cmd[0]);
+
+        $textmessage = '';
+
+        switch ($cmd[0]) {
+            case '/start':
+                $textmessage = 'Бот активирован';
+                $_SESSION['STATUS'] = true;
+                break;
+            case '/stop':
+                $_SESSION['STATUS'] = false;
+                $textmessage = 'Бот деактивирован';
+                break;
+            case '/auth':
+                if ($cmd[1] == '' || $cmd[2] == '') {
+                    $textmessage = 'Введите логин и пароль в формате:' . PHP_EOL;
+                    $textmessage .= '/auth login password' . PHP_EOL;
+                } else {
+                    $textmessage = 'Проверка авторизации' . PHP_EOL;
+                    $textmessage .= 'Выполняю операций с движком...' . PHP_EOL;
+                    $textmessage .= 'Авторизация успешна (нет)' . PHP_EOL;
+                }
+
+                break;
+            case '/help':
+                $textmessage = 'Список команд бота' . PHP_EOL;
+                $textmessage .= '/start : Запускает работу бота' . PHP_EOL;
+                $textmessage .= '/stop : Останавливает работу бота' . PHP_EOL;
+                $textmessage .= '/auth : Авторизаций бота в движке' . PHP_EOL;
+                break;
+            default:
+                $textmessage = 'Команда не найдена';
+                break;
+        }
+        Message::sendMessage($textmessage, $json['message']['chat']['id'], $json['message']['message_id'], true);
+    }
+
+    public function ExecEvents()
+    {
+
+        $json = self::getJson();
+        $type = self::getTypes();
+
+        $jsonmessage = json_encode($json);
+        /*Debug::SendLog($jsonmessage, $type);
+        Debug::SendLog('status: ' . $_SESSION['STATUS'], 'check session');*/
+
+        switch ($type) {
+            case 'bot_command':
+                self::ExecCmd($json);
+                break;
+            case 'mention':
+                $textmessage = 'Ответ на сообщение с указанием ';
+                Message::sendMessage($textmessage, $json['message']['chat']['id'], $json['message']['message_id'],
+                    true);
+                break;
+            case 'channel':
+                //Продумать логику
+                break;
+            case 'private':
+                $textmessage = 'Ответ на сообщение в личке';
+                Message::sendMessage($textmessage, $json['message']['chat']['id'], $json['message']['message_id'],
+                    true);
+                break;
+            case 'group':
+            case 'supergroup':
+                //if (!$_SESSION['STATUS']) break;
+                $textmessage = 'Ответ на сообщение в группе';
+                Message::sendMessage($textmessage, $json['message']['chat']['id'], $json['message']['message_id'],
+                    true);
+                break;
+        }
+    }
+
+}
--- web/index.php
+++ web/index.php
@@ -5,11 +5,15 @@ require('../vendor/autoload.php');
 use IronCatBot\IronCatBot\ICBot;
 use IronCatBot\IronCatBot\Types\Message;
 use IronCatBot\Debug\Debug;
+use IronCatBot\Router\Router;
 
 try {
     $bot = new ICBot();
-    $json = $bot->getJson();
-    Debug::SendLog(json_encode($json),$bot->getTypes());
+
+    $router = new Router();
+    $router->ExecEvents();
+
+    //Debug::SendLog(json_encode($json),$bot->getTypes());
 
 } catch (Exception $e) {
     echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
